06e187f8407b81ccb0d498a62ff7095e9dd5788e
Mohsen <ash.wxrz@hotmail.com>
2022-04-02 17:44:11 +0300

    Added Dynamic colors

--- app/src/main/java/com/client/aircasting/App.kt
+++ app/src/main/java/com/client/aircasting/App.kt
@@ -1,7 +1,14 @@
 package com.client.aircasting
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
