2f4466491084ae3776f01953bae3384f3ae3e578
Mohsen <ash.wxrz@hotmail.com>
2022-04-03 03:20:11 +0300

    Added empty design

--- app/src/main/java/com/client/aircasting/ui/main/view/tabs/FollowingTab.kt
+++ app/src/main/java/com/client/aircasting/ui/main/view/tabs/FollowingTab.kt
@@ -1,14 +1,15 @@
 package com.client.aircasting.ui.main.view.tabs
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,28 +20,56 @@ import com.client.aircasting.R
 fun FollowingScreen() {
     Column(
         modifier = Modifier
+            .padding(start = 24.dp, end = 24.dp)
             .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
     ) {
 
-        Box(
+        Text(
+            text = stringResource(id = R.string.dashboard_empty_header),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.aircasting_dark_blue),
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = stringResource(id = R.string.dashboard_empty_text),
+            fontSize = 18.sp,
             modifier = Modifier
-                .padding(start = 24.dp, end = 24.dp)
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 10.dp),
+            textAlign = TextAlign.Center
+        )
+
+        Button(
+            onClick = { goToLetsStart() },
+            modifier = Modifier
+                .padding(start = 20.dp, top = 20.dp, end = 20.dp)
+                .height(50.dp)
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(),
+            colors = ButtonDefaults.textButtonColors(
+                backgroundColor = colorResource(id = R.color.aircasting_blue_400)
+            )
         ) {
             Text(
-                text = stringResource(id = R.string.dashboard_empty_header),
+                text = stringResource(id = R.string.record_new_session),
                 fontSize = 18.sp,
                 textAlign = TextAlign.Center,
+                color = colorResource(id = R.color.aircasting_white)
             )
-
         }
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun FollowingScreenPreview() {
     FollowingScreen()
+}
+
+fun goToLetsStart() {
+    TODO("Not yet implemented")
 }
