9f27793341c265d3cb206b7aab5d62fb97ced154
Mohsen <ash.wxrz@hotmail.com>
2022-04-06 02:56:14 +0300

    Added implementation for register screen

--- app/src/main/java/com/client/aircasting/ui/auth/Register.kt
+++ app/src/main/java/com/client/aircasting/ui/auth/Register.kt
@@ -1,9 +1,29 @@
 package com.client.aircasting.ui.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Register() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.background)
+    ) {
 
 
+    }
+}
+
+@Preview
+@Composable
+fun ShowRegisterPreview() {
+    val navController = rememberNavController()
+    Register()
 }
