ebf2ec879ab92684dc58d1596cf61ec7e9839cab
Mohsen <ash.wxrz@hotmail.com>
2022-04-08 02:49:11 +0300

    Redesigning login screen

--- app/src/main/java/com/client/aircasting/ui/auth/view/login/Login.kt
+++ app/src/main/java/com/client/aircasting/ui/auth/view/login/Login.kt
@@ -2,21 +2,16 @@ package com.client.aircasting.ui.auth.view.login
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -26,105 +21,39 @@ import com.client.aircasting.R
 import com.client.aircasting.ui.auth.viewmodel.AuthViewModel
 
 @Composable
-fun Login(
-    navController: NavController,
-    authViewModel: AuthViewModel = hiltViewModel()
-) {
-    var profileName by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
-
+fun Login(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = MaterialTheme.colors.background)
     ) {
 
-        Text(
-            text = stringResource(id = R.string.login_header),
-            modifier = Modifier.padding(start = 30.dp, top = 40.dp),
-            fontWeight = FontWeight.Bold,
-            style = MaterialTheme.typography.h5,
-            color = colorResource(id = R.color.aircasting_blue_400)
-        )
-
-        Text(
-            text = stringResource(id = R.string.login_description),
-            modifier = Modifier.padding(start = 30.dp, top = 20.dp)
-        )
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 30.dp, top = 20.dp, end = 30.dp)
-        ) {
-
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = profileName,
-                onValueChange = {
-                    profileName = it
-                },
-                label = {
-                    Text(
-                        text = stringResource(id = R.string.profile_name_hint),
-                    )
-                }
-            )
-
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 30.dp, top = 20.dp, end = 30.dp)
-        ) {
-            var showPassword by remember { mutableStateOf(false) }
-
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = password,
-                onValueChange = {
-                    password = it
-                },
-                label = {
-                    Text(
-                        text = stringResource(id = R.string.password_hint),
-                    )
-                },
-                visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                trailingIcon = {
-                    IconButton(onClick = {
-                        showPassword = !showPassword
-                    }) {
-                        Icon(
-                            imageVector = if (showPassword)
-                                Icons.Filled.Visibility
-                            else
-                                Icons.Filled.VisibilityOff, ""
-                        )
-                    }
-                })
-        }
-
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 30.dp, top = 30.dp, end = 30.dp)
+                .fillMaxSize()
+                .padding(start = 30.dp, end = 30.dp)
         ) {
+            Text(
+                text = stringResource(id = R.string.login_header),
+                modifier = Modifier.padding(top = 40.dp),
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.h5,
+                color = colorResource(id = R.color.aircasting_blue_400)
+            )
 
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = colorResource(id = R.color.aircasting_blue_400)
-                ),
-                onClick = {
+            Text(
+                text = stringResource(id = R.string.login_description),
+                modifier = Modifier.padding(top = 20.dp)
+            )
 
-                    authViewModel.login(profileName, password)
+            TextFields()
+
+            Button(modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp)
+                .height(50.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.aircasting_blue_400)),
+                onClick = { // todo
 
                 }) {
                 Text(
@@ -139,7 +68,7 @@ fun Login(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(start = 30.dp, top = 20.dp, end = 30.dp),
+                    .padding(top = 20.dp),
                 contentAlignment = Alignment.BottomCenter
             ) {
                 Text(
@@ -149,10 +78,32 @@ fun Login(
                     ), style = MaterialTheme.typography.body1
                 )
             }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = stringResource(id = R.string.create_account_prompt),
+                    style = MaterialTheme.typography.body1
+                )
+
+                Text(
+                    text = stringResource(id = R.string.create_account_text_button),
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.padding(start = 10.dp, end = 30.dp),
+                    color = colorResource(id = R.color.aircasting_blue_400),
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
-
     }
+}
 
+
+@Composable
+fun LogicAction(profileName: String, password: String) {
+    val authViewModel: AuthViewModel = hiltViewModel()
+    authViewModel.login(profileName, password)
 }
 
 @Preview
