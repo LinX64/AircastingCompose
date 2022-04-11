a57542c66211faff70cfe86ae322d112d7293190
Mohsen <ash.wxrz@hotmail.com>
2022-04-11 06:18:44 -0700

    Added BottomBar which was forgotten as well as a few others

---
+++ app/src/main/java/com/client/aircasting/ui/main/view/componenets/BottomBar.kt
@@ -0,0 +1,48 @@
+package com.client.aircasting.ui.main.view.componenets
+
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.vectorResource
+import com.client.aircasting.R
+
+@Composable
+fun BottomBar() {
+    val selectedIndex = remember { mutableStateOf(0) }
+    NavigationBar() {
+        BottomNavigationItem(icon = {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_home_black_24dp),
+                ""
+            )
+        }, selectedContentColor = colorResource(id = R.color.aircasting_blue_400),
+            selected = (selectedIndex.value == 0),
+            onClick = {
+                selectedIndex.value = 0
+            })
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = ImageVector.vectorResource(id = R.drawable.ic_plus_black_24), "")
+        },
+            selected = (selectedIndex.value == 1),
+            onClick = {
+                selectedIndex.value = 1
+            })
+
+        BottomNavigationItem(icon = {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_settings_black_24dp),
+                ""
+            )
+        },
+            selected = (selectedIndex.value == 2),
+            onClick = {
+                selectedIndex.value = 2
+            })
+    }
+}
