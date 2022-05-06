8220393ace929070c5a1f6d425843cd6b44fde64
mohsen <ash.wxrz@hotmail.com>
2022-05-07 02:07:24 +0430

    Added helper class for supporting different screens

--- app/src/main/java/com/client/aircasting/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/client/aircasting/ui/main/view/MainActivity.kt
@@ -3,9 +3,9 @@ package com.client.aircasting.ui.main.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Surface
 import com.client.aircasting.ui.navigation.SetupNavGraph
 import com.client.aircasting.ui.theme.AircastingTheme
+import com.client.aircasting.util.initSize
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,16 +14,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initSize()
+
         setContent {
             AircastingTheme {
-                Surface {
-                    SetupNavGraph()
-                }
+                SetupNavGraph()
             }
         }
+
     }
 }
-
-
 
 
