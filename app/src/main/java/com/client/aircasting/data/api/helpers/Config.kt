f442aa7c4b61881019a1384357d7d5bee50f3454
mohsen <ash.wxrz@hotmail.com>
2022-05-07 20:37:13 +0430

    Changed the name with constants

--- app/src/main/java/com/client/aircasting/data/api/helpers/Config.kt
+++
@@ -1,7 +0,0 @@
-package com.client.aircasting.data.api.helpers
-
-object Constants {
-    const val baseUrl: String = "http://aircasting.org"
-    const val urlLogin: String = "/api/user.json"
-    const val urlGetMovies: String = "movielist.json"
-}
