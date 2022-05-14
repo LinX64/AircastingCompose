da151ca231cc7974e2990e861a4bc617fc5792a3
mohsen <ash.wxrz@hotmail.com>
2022-05-14 18:44:30 +0430

    Added resource + response handler and status

---
+++ app/src/main/java/com/client/aircasting/util/ResponseHandler.kt
@@ -0,0 +1,28 @@
+package com.client.aircasting.util
+
+import android.content.Context
+import com.client.aircasting.R
+import retrofit2.HttpException
+import javax.inject.Inject
+
+open class ResponseHandler @Inject constructor(private val mContext: Context) {
+    fun <T : Any> handleSuccess(data: T): Resource<T> {
+        return Resource.success(data)
+    }
+
+    fun <T : Any> handleException(e: Exception): Resource<T> {
+        return when (e) {
+            is HttpException -> Resource.error(null, getErrorMessage(e.code()))
+            else -> Resource.error(null, getErrorMessage(Int.MAX_VALUE))
+        }
+    }
+
+    private fun getErrorMessage(code: Int): String {
+        return when (code) {
+            401 -> mContext.getString(R.string.unauth)
+            404 -> mContext.getString(R.string.not_found)
+            500 -> mContext.getString(R.string.internal_error)
+            else -> mContext.getString(R.string.sync_error_header)
+        }
+    }
+}
---
+++ app/src/main/java/com/client/aircasting/util/Status.kt
@@ -0,0 +1,7 @@
+package com.client.aircasting.util
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
