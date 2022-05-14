5a474fa2e011f7aac0021c474e39283fc0f1573a
mohsen <ash.wxrz@hotmail.com>
2022-05-14 18:43:11 +0430

    Login password textfield fix for icon and clear focus

--- app/src/main/java/com/client/aircasting/ui/view/auth/login/components/LoginPasswordTextField.kt
+++ app/src/main/java/com/client/aircasting/ui/view/auth/login/components/LoginPasswordTextField.kt
@@ -4,12 +4,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActionScope
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -21,7 +31,7 @@ fun LoginPasswordTextField(
     onValueChange: (String) -> Unit,
     onDone: (KeyboardActionScope.() -> Unit),
 ) {
-    val showPassword by rememberSaveable { mutableStateOf(value = false) }
+    var showPassword by rememberSaveable { mutableStateOf(value = false) }
 
     OutlinedTextField(
         value = password,
@@ -33,7 +43,31 @@ fun LoginPasswordTextField(
             )
         },
         visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val (icon, iconColor) = if (showPassword) {
+                Pair(
+                    Icons.Filled.Visibility,
+                    Color.Red.copy(0.7F)
+                )
+            } else {
+                Pair(
+                    Icons.Filled.VisibilityOff,
+                    Color.DarkGray
+                )
+            }
+
+            IconButton(onClick = { showPassword = !showPassword }) {
+                Icon(
+                    icon,
+                    contentDescription = "Visibility",
+                    tint = iconColor
+                )
+            }
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
+        ),
         keyboardActions = KeyboardActions(
             onDone = onDone
         )
