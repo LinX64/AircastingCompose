fcff33a8c83c1efc0d3053831792f9a9c0b94fd9
mohsen <ash.wxrz@hotmail.com>
2022-05-15 16:54:52 +0430

    Added register auth

--- app/src/main/java/com/client/aircasting/data/api/services/ApiService.kt
+++ app/src/main/java/com/client/aircasting/data/api/services/ApiService.kt
@@ -1,15 +1,23 @@
 package com.client.aircasting.data.api.services
 
 import com.client.aircasting.data.api.helpers.Constants
+import com.client.aircasting.data.api.params.CreateAccountBody
 import com.client.aircasting.data.api.responses.UserResponse
+import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
 
-    /* Auth Requests */
+    /* GET Requests */
     @GET(Constants.urlLogin)
     suspend fun login(): Response<UserResponse>
+
+    /* POST Requests */
+    @POST(Constants.urlCreateAccount)
+    suspend fun createAccount(@Body body: CreateAccountBody): Response<UserResponse>
 }
 
 
--- app/src/main/java/com/client/aircasting/ui/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/client/aircasting/ui/viewmodel/AuthViewModel.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.client.aircasting.data.api.helpers.ApiServiceFactory
+import com.client.aircasting.data.api.params.CreateAccountBody
+import com.client.aircasting.data.api.params.CreateAccountParams
 import com.client.aircasting.util.Settings
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -22,10 +24,10 @@ class AuthViewModel @Inject constructor(
     val errorAuth = mutableStateOf(false)
 
     fun login(profileName: String, password: String) = viewModelScope.launch(Dispatchers.IO) {
-        try {
-            isLoading.value = true
-            val responseService = mApiServiceFactory.get(profileName, password).login()
+        isLoading.value = true
 
+        try {
+            val responseService = mApiServiceFactory.get(profileName, password).login()
             when {
                 responseService.isSuccessful -> {
                     delay(1500L)
@@ -50,4 +52,37 @@ class AuthViewModel @Inject constructor(
             errorMessage.value = e.message.toString()
         }
     }
+
+    fun createAccount(profileName: String, password: String, email: String, send_emails: Boolean) =
+        viewModelScope.launch(Dispatchers.IO) {
+            isLoading.value = true
+
+            try {
+                val createAccountBody = CreateAccountBody(CreateAccountParams(
+                    profileName,
+                    password,
+                    email,
+                    send_emails
+                ))
+                val responseService = mApiServiceFactory.get(emptyList())
+                val response = responseService.createAccount(createAccountBody)
+
+                when {
+                    response.isSuccessful -> {
+
+                        isSuccessLoading.value = true
+                        isLoading.value = false
+                    }
+                    else -> response.errorBody()?.let { error ->
+
+
+                    }
+                }
+
+                isLoading.value = false
+            } catch (e: Exception) {
+                isLoading.value = false
+                errorMessage.value = e.message.toString()
+            }
+        }
 }
