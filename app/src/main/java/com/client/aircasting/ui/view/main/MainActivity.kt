efbd66ef77f7adb9154b02eac3fe06e16454752c
Mohsen <ash.wxrz@hotmail.com>
2022-07-17 14:59:25 +0200

    Removed windowsmanager

--- app/src/main/java/com/client/aircasting/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/client/aircasting/ui/view/main/MainActivity.kt
@@ -3,11 +3,8 @@ package com.client.aircasting.ui.view.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import com.client.aircasting.ui.viewmodel.AuthViewModel
 import com.client.aircasting.ui.view.navigation.SetupNavGraph
 import com.client.aircasting.ui.view.theme.AircastingTheme
-import com.client.aircasting.util.initSize
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,8 +12,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        initSize()
 
         setContent {
             AircastingTheme {
--- app/src/main/java/com/client/aircasting/ui/view/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/client/aircasting/ui/view/navigation/SetupNavGraph.kt
@@ -3,7 +3,6 @@ package com.client.aircasting.ui.view.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.client.aircasting.ui.viewmodel.AuthViewModel
 import com.client.aircasting.ui.view.navigation.nav_graphs.authNavGraph
 import com.client.aircasting.ui.view.navigation.nav_graphs.homeNavGraph
 import com.client.aircasting.ui.view.navigation.nav_graphs.introNavGraph
@@ -11,6 +10,7 @@ import com.client.aircasting.ui.view.navigation.nav_graphs.introNavGraph
 @Composable
 fun SetupNavGraph() {
     val navController = rememberNavController()
+
     NavHost(
         navController = navController,
         startDestination = INTRO_GRAPH_ROUTE,
